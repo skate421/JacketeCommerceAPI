16e9743732f1ec2b86c3e103e69bbfb06ec835b5
Skate421 <SKATIE13@outlook.com>
2024-12-07 01:08:45 -0400

    commitWCors

--- app.js
+++ app.js
@@ -1,24 +1,35 @@
 import express from 'express';
-import userRouter from './routes/users.js';
-import productRouter from './routes/products.js';
 import cors from 'cors';
 import session from 'express-session';
+import userRouter from './routes/users.js';
+import productRouter from './routes/products.js';
 
 const port = process.env.PORT || 3000;
 const app = express();
 
-//Middleware
+// middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.use(cors());
 
+// cors middleware
+app.use(cors({
+  origin: 'http://localhost:5173', // react client
+  credentials: true // allow cookies
+}));
+
+// express-session middleware
 app.use(session({
-  secret: process.env.SECRET_KEY,
+  secret: 'fkldjbnfdkFTFT5efd3$$sdg89F',
   resave: false,
   saveUninitialized: true,
-  cookie: { secure: false }
-}))
+  cookie: { 
+    httpOnly: true,
+    secure: false,  // Set to `true` if using HTTPS in production
+    sameSite: 'lax',  // Consider 'none' if client and server are on different origins
+    maxAge: 3600000 // 1 hour in milliseconds
+  }
+}));
 
 app.use('/api/jacket/users', userRouter);
 app.use('/api/jacket/products', productRouter);
